33072e3ef4b411bb4ab1a2d42132d10fec420f87
garinich777 <62898482+garinich777@users.noreply.github.com>
2021-05-14 11:38:45 +0300

    Добавьте файлы проекта.

---
+++ TranslatorClipboard/Program.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+
+namespace TranslatorClipboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Тема лох";
+            var html = new HtmlDocument();
+            using var wc = new WebClient();
+            string htmlText = wc.DownloadString(@$"https://translate.google.com/?sl=ru&tl=en&text={ text }&op=translate");
+            html.LoadHtml(htmlText);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
